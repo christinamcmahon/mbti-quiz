c45c4423a0e7e157caaf76d71316d31bd75468d2
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 14:10:50 -0800

    add new and create to Answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+    def new 
+        @answer = Answer.new
+    end
+
+    def create 
+        @answer = Answer.new(answer_params)
+        if @answer.save 
+            # redirect_to personality_path(@answer)???
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def answer_params
+        params.require(:answer).permit(:introverted, :observant, :thinking, :judging)
+    end
 end
