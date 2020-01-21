5a22a9d0c1397e520f66db01032edf9c9c761188
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 11:53:18 -0800

    rails resource Friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+    
+    def index 
+        @users = User.all
+    end
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else 
+            render :new
+        end
+    end
+
+    def update 
+        if @user.update(user_params)
+            redirect_to @user
+        else 
+            render :edit
+        end
+    end
+
+    def destroy 
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+
+    def find_user 
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :bio)
+    end
+
+
 end
