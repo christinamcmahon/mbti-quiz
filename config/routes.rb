a4346dc4593ad21126f34912f119425d3e313478
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 12:53:38 -0800

    limit routes for Answer and Personality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :personalities
-  resources :answers
+  resources :personalities, only: [:index, :show]
+  resources :answers, only: [:new, :create]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
