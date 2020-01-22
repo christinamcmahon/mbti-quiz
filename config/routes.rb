005383f41294dab14aef3449a08ea067130dffa3
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-22 09:14:05 -0800

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :answers, only: [:new, :create]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  root to: "personalities#index"
 
   get '/signup', to: "users#new", as: "signup"
   
