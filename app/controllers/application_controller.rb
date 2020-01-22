ff642daaa250e272c87bef49aa444998abe4bf2e
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 17:18:38 -0800

    add overall helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :check_user
+
+    def check_user
+        if !logged_in?
+            redirect_to login_path
+        end
+    end
+
+    def logged_in?
+        !!sessions[:user_id]
+    end
 end
