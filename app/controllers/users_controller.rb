c90a8d62454a2ef13ae33fa3a82d2f39946ad532
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 17:19:39 -0800

    add error message to User create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
         if @user.save
             redirect_to @user
         else 
+            flash[:message] = @user.full_messages
             render :new
         end
     end
