5bbbdfd0e89c939b27b08cb5a2f831e97ea28dc9
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 17:20:37 -0800

    setup routes for signup/login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :answers, only: [:new, :create]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "personalities#index"
+
+  get '/signup', to: "user#new", as: "signup"
+  
+  get '/login', to: "auth#login", as: "login"
+  post '/login', to: "auth#authenticate"
+
+  get '/logout', to: "auth#logout"
 end
