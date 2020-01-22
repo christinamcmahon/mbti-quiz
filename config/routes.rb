612a8be94188c60d27b7bfe95e332f94b6df8e32
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 18:11:46 -0800

    edit signup route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :answers, only: [:new, :create]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "personalities#index"
+  
 
-  get '/signup', to: "user#new", as: "signup"
+  get '/signup', to: "users#new", as: "signup"
   
   get '/login', to: "auth#login", as: "login"
   post '/login', to: "auth#authenticate"
