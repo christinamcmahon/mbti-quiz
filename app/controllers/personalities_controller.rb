70c2df3f4fb8a9da371cd4f820e3283d78178438
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 16:47:24 -0800

    add index and show to Personality controller

--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -1,2 +1,9 @@
 class PersonalitiesController < ApplicationController
+    def index 
+        @personalities = Personality.all
+    end
+
+    def show 
+        @personality = Personality.find(params[:id])
+    end
 end
