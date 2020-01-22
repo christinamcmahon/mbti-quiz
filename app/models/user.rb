6cf8ac6ea3e4fc7b1d5466f924cff13063df0b67
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-21 18:14:52 -0800

    add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
     has_many :answers, through: :answers_users
     has_many :answers_users
     belongs_to :personality
+
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :bio, presence: true
+
+    def calculatePersonality
+
+    end
 end
