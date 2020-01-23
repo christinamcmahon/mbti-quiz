fd9d56c98d1dd0aa30c51ebd38105235c251235c
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-23 12:04:43 -0800

    edit creates

--- app/controllers/answers_users_controller.rb
+++ app/controllers/answers_users_controller.rb
@@ -1,15 +1,11 @@
 class AnswersUsersController < ApplicationController
     def question1 
         @answer = Answer.all[0]
-        
     end
 
     def create_q1
-
-        # @answer = Answer.all[0]
-
-        @answers_user = AnswersUser.new(user_id: current_user.id, response: params["/quiz_question_1"]["response"])
-        byebug
+        @answer = Answer.all[0]
+        @answers_user = AnswersUser.new(user_id: current_user.id, response: params["/quiz_question_1"]["response"], answer_id: @answer.id)
         if @answers_user.save 
             redirect_to quiz_question_2_path
         else
@@ -19,11 +15,11 @@ class AnswersUsersController < ApplicationController
 
     def question2
         @answer = Answer.all[1]
-        @answers_user =  AnswersUser.new
     end
 
     def create_q2
-        @answers_user = AnswersUser.new(user_id: current_user.id, response: session[:response])
+        @answer = Answer.all[1]
+        @answers_user = AnswersUser.new(user_id: current_user.id, response: params["/quiz_question_2"]["response"], answer_id: @answer.id)
         if @answers_user.save 
             redirect_to quiz_question_3_path
         else
@@ -33,11 +29,11 @@ class AnswersUsersController < ApplicationController
 
     def question3
         @answer = Answer.all[2]
-        @answers_user =  AnswersUser.new
     end
 
     def create_q3
-        @answers_user = AnswersUser.new(user_id: current_user.id, response: session[:response])
+        @answer = Answer.all[2]
+        @answers_user = AnswersUser.new(user_id: current_user.id, response: params["/quiz_question_3"]["response"], answer_id: @answer.id)
         if @answers_user.save 
             redirect_to quiz_question_4_path
         else
@@ -47,11 +43,11 @@ class AnswersUsersController < ApplicationController
 
     def question4
         @answer = Answer.all[3]
-        @answers_user =  AnswersUser.new
     end
 
     def create_q4
-        @answers_user = AnswersUser.new(user_id: current_user.id, response: session[:response])
+        @answer = Answer.all[3]
+        @answers_user = AnswersUser.new(user_id: current_user.id, response: params["/quiz_question_4"]["response"], answer_id: @answer.id)
         if @answers_user.save 
             redirect_to user_path(current_user)
         else
