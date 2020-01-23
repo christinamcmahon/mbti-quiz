5bf034649115a6c71090fd862a069af24353d722
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-22 19:06:36 -0800

    edit current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?, :require_login, :current_user
 
     def current_user
-        return unless session[:user_id]
-        @user ||= User.find(session[:user_id])
+        User.find_by(id: session[:user_id])
     end
 
     def require_login
